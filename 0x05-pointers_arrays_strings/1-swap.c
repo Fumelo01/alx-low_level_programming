ba61cd540f4c1e3390b608da0d019d9a6c2d754b
Oluwafunmilayo <oluwafunmilayoa2@gmail.com>
2022-10-25 23:10:48 -0700

    ALX Programming

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ * swap_int - entry point
+ * @a: first pointer variable
+ * @b: second pointer variable
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+	a = *b;
+	b = *a;
+}
