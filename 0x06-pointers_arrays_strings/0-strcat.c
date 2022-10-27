7747fe73dc3a2f4eed0dea3f8ab5a29a57c6b2c7
Oluwafunmilayo <oluwafunmilayoa2@gmail.com>
2022-10-27 05:22:34 -0700

    ALX Programming

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ * @dest: first argument
+ * @src: second argument
+ *
+ * Return: destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[i] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
