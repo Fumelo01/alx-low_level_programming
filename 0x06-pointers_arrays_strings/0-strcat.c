a1f2eba8f23d65ac0a40c86b4071a4b79faa13f5
Oluwafunmilayo <oluwafunmilayoa2@gmail.com>
2022-10-27 17:07:55 -0700

    ALX Programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,9 +14,12 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
+
 	for (j = 0; src[i] != '\0'; i++, j++)
 	{
+		printf("\ndest before:%s\n src before: %s\n", dest, src);
 		dest[i] = src[j];
+		printf("\ndest after: %s\n src after: %s\n", dest, src);
 	}
 	dest[i] = '\0';
 	return (dest);
